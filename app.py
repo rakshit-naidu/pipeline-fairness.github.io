52839915aed9d5a38126c1e9c2c3915a80e0ccd2
rakshit-naidu <rakshit.naidu07@gmail.com>
2023-10-30 11:57:06 -0400

    fixing the links #4

--- app.py
+++ app.py
@@ -11,11 +11,11 @@ app = Flask(__name__)
 def home():
     return render_template('index_b4_login.html')
 
-@app.route('/about', methods=['GET', 'POST'])
+@app.route('/aboutus', methods=['GET', 'POST'])
 def aboutpage():
     return render_template('about.html')
 
-@app.route('/contact', methods=['GET', 'POST'])
+@app.route('/contactus', methods=['GET', 'POST'])
 def contactpage():
     return render_template('contact.html')
 
